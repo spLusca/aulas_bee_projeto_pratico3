ef408de7bd7ed66c4adb4a32b0c725fbe72bb10a
spLusca <lucaspinheiros2@hotmail.com>
2023-10-28 23:59:10 -0300

    Projeto Pratico 3

---
+++ src/main/java/com/ambev/projetopratico3/Controller/ProdutoController.java
@@ -0,0 +1,28 @@
+package com.ambev.projetopratico3.Controller;
+
+import com.ambev.projetopratico3.Model.Produto;
+import com.ambev.projetopratico3.Repository.ProdutoRepository;
+import com.ambev.projetopratico3.Service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @PostMapping("/produtos")
+    public ResponseEntity <Produto> salvarProduto(Produto produto) {
+
+        Produto produtoSalvo = produtoService.salvarProduto(produto);
+        return ResponseEntity.ok(produtoSalvo);
+    }
+}
